f89e7d88b65b0082e7c1187472eba49d0c5f5288
Mara Neagu <marateodoraneagu@gmail.com>
2024-05-25 11:31:26 +0300

    found an error at the user validator

--- RecipeProject/middlewares/userValidator.js
+++ RecipeProject/middlewares/userValidator.js
@@ -1,7 +1,6 @@
 const { body, validationResult } = require('express-validator');
 
 const updateUser = [
-    body('name').optional().notEmpty().withMessage('Name cannot be empty!'),
     body('password').optional().isLength({ min: 8 }).withMessage('Password must be at least 8 characters long!'),
     
     (req, res, next) => {
